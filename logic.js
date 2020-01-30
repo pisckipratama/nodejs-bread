4ff7e64c116648d5926d758987430cad4b9a76c9
Piscki Pratama <pisckipratama@gmail.com>
2020-01-30 23:36:41 +0700

    update fitur dosen: browse, search, add. mahasiswa: search, add

--- logic.js
+++ logic.js
@@ -42,6 +42,15 @@ silahkan pilih opsi di bawah ini
 [5] kembali
 ${space}`
 
+let listDosen = `${space}
+silahkan pilih opsi di bawah ini
+[1] daftar dosen
+[2] cari dosen
+[3] tambah dosen
+[4] hapus dosen
+[5] kembali
+${space}`
+
 function menuMhs() {
     console.log(listMhs);
     rl.question('masukkan opsi: ', (answer) => {
@@ -49,7 +58,7 @@ function menuMhs() {
             case '1':
                 let table = new Table({
                     head: ['NIM', 'Nama', 'alamat', 'jurusan'],
-                    colWidths: [7, 20, 10, 10]
+                    colWidths: [7, 20, 15, 10]
                 })
                 db.serialize(() => {
                     let sql = 'select * from mahasiswa';
@@ -93,8 +102,153 @@ function menuMhs() {
                 break;
 
             case '3':
-                console.log('tambah mah nanti wkwkwkwkwwk');
+                let table2 = new Table({
+                    head: ['NIM', 'Nama', 'alamat', 'jurusan'],
+                    colWidths: [7, 20, 15, 10]
+                })
+                console.log(space);
+                console.log('lengkapi data dibawah ini: ');
+                let forAddMhs = {
+                    nim: 0,
+                    name: '',
+                    jurusan: 0,
+                    alamat: ''
+                };
+                rl.question('nim: ', (jawab) => {
+                    forAddMhs.nim = parseInt(jawab);
+                    rl.question('nama: ', (jawab) => {
+                        forAddMhs.name = jawab;
+                        rl.question('jurusan: ', (jawab) => {
+                            forAddMhs.jurusan = parseInt(jawab);
+                            rl.question('alamat: ', (jawab) => {
+                                forAddMhs.alamat = jawab;
+                                console.log(space);
+                                db.serialize(() => {
+                                    let sql = `insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(${forAddMhs.nim}, '${forAddMhs.name}', '${forAddMhs.alamat}', ${forAddMhs.jurusan})`
+                                    db.run(sql, (err) => {
+                                        if (err) throw err;
+                                        let sql = 'select * from mahasiswa';
+                                        db.all(sql, (err, rows) => {
+                                            if (err) throw err;
+                                            if (rows) {
+                                                rows.forEach(mhs => {
+                                                    table2.push(
+                                                        [`${mhs.nim}`, `${mhs.nama_mahasiswa}`, `${mhs.alamat}`, `${mhs.id_jurusan}`]
+                                                    )
+                                                })
+                                                console.log(`${table2.toString()}`);
+                                                menuMhs();
+                                            }
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+                break;
+
+            case '4':
+                console.log('hapus mah nanti wkwkwkwkwwk');
                 menuMhs();
+                break;
+            case '5':
+                menu();
+                break;
+            default:
+                console.log('List yang dimaksud tidak ada');
+                menuMhs();
+                break;
+        }
+    })
+}
+
+function menuDosen() {
+    console.log(listDosen);
+    rl.question('masukkan opsi: ', (answer) => {
+        switch (answer) {
+            case '1':
+                let table = new Table({
+                    head: ['ID', 'Nama'],
+                    colWidths: [7, 20]
+                })
+                db.serialize(() => {
+                    let sql = 'select * from dosen';
+                    db.all(sql, (err, rows) => {
+                        if (err) throw err;
+                        if (rows) {
+                            rows.forEach(dosen => {
+                                table.push(
+                                    [`${dosen.id_dosen}`, `${dosen.nama_dosen}`]
+                                )
+                            })
+                            console.log(`${table.toString()}`);
+                            menuDosen();
+                        }
+                    })
+                });
+                break;
+
+            case '2':
+                console.log(space);
+                rl.question('masukkan ID dosen: ', (ans) => {
+                    db.serialize(() => {
+                        let sql = `select * from dosen where id_dosen=${parseInt(ans)}`;
+                        db.all(sql, (err, rows) => {
+                            if (err) throw err;
+                            if (rows.length === 1) {
+                                console.log(space);
+                                console.log('dosen details')
+                                console.log(space);
+                                rows.forEach(dosen => {
+                                    console.log(`ID\t: ${dosen.id_dosen}\nnama\t: ${dosen.nama_dosen}`);
+                                })
+                                menuDosen();
+                            } else {
+                                console.log(`dosen dengan ID ${ans} tidak terdaftar`);
+                                menuDosen();
+                            }
+                        })
+                    });
+                })
+                break;
+
+            case '3':
+                let table2 = new Table({
+                    head: ['ID', 'Nama'],
+                    colWidths: [7, 20]
+                })
+                console.log(space);
+                console.log('lengkapi data dibawah ini: ');
+                let forAddDosen = {
+                    id: 0,
+                    name: '',
+                };
+                rl.question('ID: ', (answer) => {
+                    forAddDosen.id = parseInt(answer);
+                    rl.question('Nama: ', (answer) => {
+                        forAddDosen.name = answer;
+                        db.serialize(() => {
+                            let sql = `insert into dosen(id_dosen, nama_dosen) values(${forAddDosen.id}, '${forAddDosen.name}')`;
+                            db.run(sql, (err) => {
+                                if (err) throw err;
+                                let sql = 'select * from dosen';
+                                db.all(sql, (err, rows) => {
+                                    if (err) throw err;
+                                    if (rows) {
+                                        rows.forEach(dosen => {
+                                            table2.push(
+                                                [`${dosen.id_dosen}`, `${dosen.nama_dosen}`]
+                                            )
+                                        })
+                                        console.log(`${table2.toString()}`);
+                                        menuDosen();
+                                    }
+                                })
+                            })
+                        })
+                    })
+                })
                 break;
             case '4':
                 console.log('hapus mah nanti wkwkwkwkwwk');
@@ -118,16 +272,21 @@ const menu = () => {
             case '1':
                 menuMhs();
                 break;
+
+            case '3':
+                menuDosen();
+                break;
+
             case '6':
-                console.log('=================================================')
+                console.log(space)
                 console.log('Bye!');
                 console.log(msg);
                 rl.question('username: ', (answer) => {
                     if (answer === 'piscki') {
-                        console.log('=================================================')
+                        console.log(space)
                         rl.question('password: ', (answer) => {
                             if (answer === '12345') {
-                                console.log('=================================================')
+                                console.log(space)
                                 console.log('\nWelcome piscki. Your access is: ADMIN')
                                 menu();
                             } else {
@@ -153,10 +312,10 @@ console.log(msg);
 
 rl.question('username: ', (answer) => {
     if (answer === 'piscki') {
-        console.log('=================================================')
+        console.log(space)
         rl.question('password: ', (answer) => {
             if (answer === '12345') {
-                console.log('=================================================')
+                console.log(space)
                 console.log('\nWelcome piscki. Your access is: ADMIN')
                 menu();
             } else {
